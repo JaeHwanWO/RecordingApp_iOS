ebaa02ae401fb3b969ad7ddd29bd7487886c8c8d
Yejin_Cho <yy991125@naver.com>
2019-10-17 19:09:20 +0900

    colorSet 추가함

--- recordingApp/View Controllers/ScheduleViewController.swift
+++ recordingApp/View Controllers/ScheduleViewController.swift
@@ -16,11 +16,10 @@ class ScheduleViewController: UIViewController {
   // 계획!
   
   /*
-   
   한시간은 collection view 높이 / 10 정도로 하고,
    collection view는 위아래 스크롤 가능하게 한다.
    
-   공백은 자기 셀 높이! 안에서 해결한다. inner공백으로! 여백을 따로 주면 안됨.
+  공백은 자기 셀 높이! 안에서 해결한다. inner공백으로! 여백을 따로 주면 안됨.
   
   셀을 요일별로 분류한다.
   월~금 제일 빠른 시간 중, 가장 빠른 시간을 찾는다.
@@ -29,6 +28,10 @@ class ScheduleViewController: UIViewController {
   월~금 중 제일 늦은 시간까지 채우면 끝!
   */
   
+  let colorBlue = [UIColor(named: "Blue_1"), UIColor(named:"Blue_2")]
+  let colorGreen = [UIColor(named:"Green_1"), UIColor(named:"Green_2")]
+  let colorOrange = [UIColor(named:"Orange_1"), UIColor(named:"Orange_2")]
+
   @IBOutlet weak var upButton: UIButton!
   @IBOutlet weak var downButton: UIButton!
   @IBOutlet weak var timeTable: UICollectionView!
